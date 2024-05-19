8247ee4ca8e454cc7fb7950466ef93d797f63918
Chelsea Ng <Chelseang40@Gmail.com>
2024-05-19 13:32:42 +0700

    feat: initial template

---
+++ lib/pages/pokemon_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PokemonDetailPage extends StatefulWidget {
+  const PokemonDetailPage({super.key});
+
+  @override
+  State<PokemonDetailPage> createState() => _PokemonDetailPageState();
+}
+
+class _PokemonDetailPageState extends State<PokemonDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
